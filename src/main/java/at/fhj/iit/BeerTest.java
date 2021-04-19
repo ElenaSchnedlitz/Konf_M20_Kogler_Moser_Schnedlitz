2cd310582703320d8b2351c5f315f5f178ddeb44
MoeJabu <moe.jabu@gmail.com>
2021-04-19 16:27:38 +0200

    implemented beer test cases

--- src/main/java/at/fhj/iit/BeerTest.java
+++ src/main/java/at/fhj/iit/BeerTest.java
@@ -4,8 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BeerTest {
 
+    public Beer beerTest;
+
     @org.junit.jupiter.api.BeforeEach
     void setUp() {
+        beerTest = new Beer("TestBeer","TestCompany","TestType",500,4.7);
     }
 
     @org.junit.jupiter.api.AfterEach
@@ -14,29 +17,41 @@ class BeerTest {
 
     @org.junit.jupiter.api.Test
     void getBrand() {
+        assertEquals(beerTest.getBrand(),"TestCompany","getBrand should deliver brandname");
     }
 
     @org.junit.jupiter.api.Test
     void getType() {
+        assertEquals(beerTest.getType(),"TestType","getType should deliver type of beer");
     }
 
     @org.junit.jupiter.api.Test
     void getVolume() {
+        assertEquals(beerTest.getVolume(),500.0,"getVolume should return volume of beer");
     }
 
     @org.junit.jupiter.api.Test
     void getAlcoholPercent() {
+        assertEquals(beerTest.getAlcoholPercent(),4.7,"getAlcoholPercent should deliver alcohol " +
+                "percentage of beer");
     }
 
     @org.junit.jupiter.api.Test
     void isAlcoholic() {
+        assertEquals(beerTest.isAlcoholic(),true,"should return if beer is alcoholic");
     }
 
     @org.junit.jupiter.api.Test
     void takeASip() {
+        double volumeBeforSip = beerTest.getVolume();
+        beerTest.takeASip();
+        assertEquals(beerTest.getVolume(),volumeBeforSip-40,"should reduce amount of beer by 40");
     }
 
     @org.junit.jupiter.api.Test
     void testToString() {
+        String testString = beerTest.toString();
+        assertEquals(testString,"This is a beer called " + beerTest.getBrand()+" "+beerTest.getType() +
+                " with " + beerTest.getAlcoholPercent() + " percent alcohol by volume");
     }
 }
