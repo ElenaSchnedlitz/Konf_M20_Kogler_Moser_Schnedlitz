350cdd47108fc0ff5d72cfbe13e53e8a13afa610
Elena Schnedlitz <elena.schnedlitz@edu.fh-joanneum.at>
2021-04-19 15:36:03 +0200

    added a test class for wine

---
+++ src/main/java/at/fhj/iit/WineTest.java
@@ -0,0 +1,42 @@
+package at.fhj.iit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WineTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBrand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getType() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAlcoholPercent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isAlcoholic() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void takeASip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
