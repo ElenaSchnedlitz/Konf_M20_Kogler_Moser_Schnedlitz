5aea434112be828a4144e59b518b613310e75a7d
Tobias Kogler <tobias.kogler@edu.fh-joanneum.at>
2021-04-19 15:44:12 +0200

    createt ginTestClass

---
+++ src/main/java/at/fhj/iit/GinTest.java
@@ -0,0 +1,38 @@
+package at.fhj.iit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GinTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBrand() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getVolume() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAlcoholPercent() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isAlcoholic() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void takeASip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
