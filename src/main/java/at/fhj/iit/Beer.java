9e6f35909312c375c60ce965eaa6ed5b3dd8cd68
MoeJabu <moe.jabu@gmail.com>
2021-04-23 11:39:15 +0200

    added JavaDoc to beer class

--- src/main/java/at/fhj/iit/Beer.java
+++ src/main/java/at/fhj/iit/Beer.java
@@ -7,6 +7,16 @@ public class Beer extends Drink{
     private double alcoholPercent;
 
     //Constructor
+
+    /**
+     * @Obvious
+     * creates a new instnce of beer object
+     * @param name  the name of the beer
+     * @param brand the name of the brand/brewery
+     * @param type  the type of the beer, eg. Märzen, Lager, RedAle,....
+     * @param volume the fill level of the bottle/glass/can/... in ml
+     * @param alcoholPercent the alcohol content in percent
+     */
     Beer(String name,String brand, String type, double volume, double alcoholPercent){
     	super(name);
         this.brand = brand;
@@ -16,25 +26,49 @@ public class Beer extends Drink{
     }
 
     //Getter
-
+    /**
+     * @Obvious
+     * returns Name of the brand/brewery
+     * @return the name of the brand/brewery as String
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * @Obvious
+     * returns the type of beer. e.g.: Märzen, Lager, RedAle,....
+     * @return the type of beer as String
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * @Obvious
+     * returnes the fill level of the container in ml
+     * @return fill level of the container as double
+     */
     @Override
     public double getVolume(){
         return this.volume;
     }
 
+    /**
+     * @Obvious
+     * returnes the alcohol content in percent
+     * @return  the alcohol content in percent as double
+     */
     @Override
     public double getAlcoholPercent(){
         return this.alcoholPercent;
     }
 
+    /**
+     * @Obvious
+     * returnes if the beer is alcoholic
+     * @return if beer is alcoholic as boolean
+     */
     @Override
     public boolean isAlcoholic(){
         if(this.alcoholPercent < 0.5){
@@ -45,10 +79,19 @@ public class Beer extends Drink{
     }
 
     //Functions
+
+    /**
+     * Reduces the fill level of the actual object by 40 ml.
+     * is supposed to represent the drinking of a sip
+     */
     public void takeASip(){
         this.volume = this.volume - 40;
     }
-    
+
+    /**
+     * returns a sentence that sums up the most important characteristics of the beer.
+     * @return a sum up of the beer as String
+     */
     public String toString() {
     	return "This is a beer called " + brand +" "+type + " with " + this.getAlcoholPercent() + " percent alcohol by volume";
     }
