5ed437f56212783b92a650981b9ab643d39514e0
Elena Schnedlitz <elena.schnedlitz@edu.fh-joanneum.at>
2021-04-23 10:03:45 +0200

    added new JavaDoc comments for getter and setters

--- src/main/java/at/fhj/iit/Wine.java
+++ src/main/java/at/fhj/iit/Wine.java
@@ -33,25 +33,48 @@ public class Wine extends Drink {
 
     //Getter
 
+    /**
+     * returns the brand that was given for a wine object
+     * @return brand as a String
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * return the type of an wine object
+     * @return type as a String
+     */
     public String getType() {
         return type;
     }
 
     //@Override
+
+    /**
+     * returns the volume of an wine bottle of a given wine
+     * @return volume as double
+     */
     public double getVolume(){
         return this.volume;
     }
 
     //@Override
+
+    /**
+     * return the alcohol percentage of a bottle of the given wine
+     * @return alcohol percentage as double
+     */
     public double getAlcoholPercent(){
         return this.alcoholPercent;
     }
 
     //@Override
+
+    /**
+     * returns true if the given wine has an higher alcohol percentage then 0.5 per mille
+     * @return true if the accusation is correct, else false
+     */
     public boolean isAlcoholic(){
         if(this.alcoholPercent < 0.5){
             return false;
@@ -61,10 +84,18 @@ public class Wine extends Drink {
     }
 
     //Functions
+
+    /**
+     * function that visualizes a sip that has been taken by a person
+     */
     public void takeASip(){
         this.volume = this.volume - 40;
     }
 
+    /**
+     * transform the given pieces of information into a string
+     * @return contains the information about an wine object
+     */
     public String toString() {
         return "This is a wine called " + brand +" "+type + " with " + this.getAlcoholPercent() + " percent alcohol by volume";
     }
